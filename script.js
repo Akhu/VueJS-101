1edf7dff4d57f01059fae8b6358fa9d55f6c32aa
Anthony Da Cruz <me@anthony-dacruz.com>
2023-07-25 14:05:37 +0200

    Starter : ok

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //A toi de jouer ! https://youtu.be/1dLcnxIock0
+
+const MyTodoList = {
+  data() {
+    return {
+     newTask: {
+        title: '',
+        completed: false
+     },
+     todos: [
+       {
+         title: 'Faire les courses',
+          completed: false
+       }
+     ]
+    }
+  },
+  methods: {
+    addTask() {
+      this.todos.push(this.newTask)
+      this.newTask = {}
+    }
+  }
+
+}
+
+Vue.createApp(MyTodoList).mount('#app')
