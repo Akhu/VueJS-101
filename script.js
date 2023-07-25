c5f2c731f1eb91b6777e2a071f033172d4874520
Anthony Da Cruz <me@anthony-dacruz.com>
2023-07-25 14:07:26 +0200

    Final : ok

--- script.js
+++ script.js
@@ -1,27 +1,55 @@
-//A toi de jouer ! https://youtu.be/1dLcnxIock0
-
-const MyTodoList = {
+const TodoListApp = {
   data() {
     return {
-     newTask: {
-        title: '',
+      todos: [],
+      sorted: false,
+      newTask: {
+        description: null,
+        priority: 3,
         completed: false
-     },
-     todos: [
-       {
-         title: 'Faire les courses',
-          completed: false
-       }
-     ]
+      },
     }
   },
-  methods: {
-    addTask() {
-      this.todos.push(this.newTask)
-      this.newTask = {}
-    }
-  }
+  beforeCreate() {
 
+  },
+  created() {
+    this.fetchTodos();
+  },
+
+  updated() {
+    this.setTodos();
+  },
+  methods: {
+    taskColor(priority) {
+      if (priority === 1) {
+        return 'bg-red-200';
+      }
+    },
+    sortByPriority() {
+      this.todos.sort((a, b) => {
+        return a.priority - b.priority ;
+      });
+    },
+    setTodos() {
+      localStorage.setItem('todos', JSON.stringify(this.todos));
+    },
+    fetchTodos() {
+      this.todos = JSON.parse(localStorage.getItem('todos')) ?? [];
+    },
+    addNewTask() {
+      this.todos.push(this.newTask);
+      this.newTask = {
+        description: null,
+        priority: 3,
+        completed: false
+      }
+    },
+    removeAll() {
+      this.todos = []
+    }
+  },
 }
 
-Vue.createApp(MyTodoList).mount('#app')
+
+Vue.createApp(TodoListApp).mount('#app');
